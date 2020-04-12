d8b3de0d2e397de061a68bf8c34aa04a4fcbda5c
Ubuntu <dpolob@gmail.com>
2020-04-12 16:04:47 +0000

    funcionando con argumentos en la funcion

--- hello_client.py
+++ hello_client.py
@@ -6,13 +6,13 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-trans = TSocket.TSocket("localhost", 4001)
+trans = TSocket.TSocket("ec2-35-180-69-6.eu-west-3.compute.amazonaws.com", 4001)
 trans = TTransport.TBufferedTransport(trans)
 proto = TBinaryProtocol.TBinaryProtocol(trans)
 client = HelloSrv.Client(proto)
 
 trans.open()
-msg = client.hello_fun()
+msg = client.hello_fun(12.3455)
 print("[CLIENT] recieved %s" % msg)
 trans.close()
 
